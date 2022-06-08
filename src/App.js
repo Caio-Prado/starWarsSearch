8ba1ee9a1b6b5a83dce74ece0adcbb04ea0ec313
leandro-morilhas <contato@leandromorilhas.com.br>
2022-06-08 10:18:07 -0300

    estilizando component Character e ajuste no app.css

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import React from 'react';
 import './App.css';
 
 import Header from "./components/header/Header"
+import CardCharacters from './components/characters/CardCharacters';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
+      <div className="content">
+      <CardCharacters />
+      <CardCharacters />
+      <CardCharacters />
+      <CardCharacters />
+      </div>
     </div>
   );
 }
---
+++ src/components/characters/CardCharacters.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+import "./CardCharacters.css"
+
+export default function CardCharacters() {
+    return (
+        <div className="card-character">
+            <div>
+                <div className="image"></div>
+                <div className="card-detail">
+                    <h1>Jedai</h1>
+                    <h2>naves pilotadas</h2>
+                </div>
+            </div>
+        </div>
+    )
+}
