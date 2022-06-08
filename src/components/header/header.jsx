4f504b4b13e8d844c720b8487278df846e6c9044
leandro-morilhas <contato@leandromorilhas.com.br>
2022-06-08 09:50:47 -0300

    estilizando logo header e criado component characters

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import logostarWars from '../../img/logoStarWars.png'
 
 import "./Header.css"
 
 export default function Header(props) {
     return (
         <div className="header">
-            <div className="logo">Star Wars</div>
+            <img src={logostarWars} alt="logo" />
             <input type="text" name="search"  id="search"/>
         </div>
     )
