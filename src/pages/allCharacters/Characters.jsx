bac96f79492d6a9f41f3e5ea31387656ead57ea3
Caio-Prado <caio100xa@gmail.com>
2022-06-11 10:28:37 -0300

    ajustes de responsividade e consumindo api

--- src/pages/allCharacters/Characters.jsx
+++ src/pages/allCharacters/Characters.jsx
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 import "./Characters.css"
 import CardCharacters from '../../components/characters/CardCharacters';
 
 
 export default function AllCharacters() {
-   return(
+
+    const [personagens, setPersonagens] = useState([])
+
+    useEffect( () => { load() }, [] )
+
+    async function load() {
+        try {
+            const response = await axios.get(`https://swapi.dev/api/people/?page=${1}`)
+            setPersonagens(response.data.results)
+        }catch(erro) {
+            console.log(erro)
+        }
+    }
+
+   return (
     <div className="content">
-        <CardCharacters />
-        <CardCharacters />
-        <CardCharacters />
-        <CardCharacters />
+        {personagens.map( (personagem) => { return <CardCharacters /> } )}
     </div>
    );
 
