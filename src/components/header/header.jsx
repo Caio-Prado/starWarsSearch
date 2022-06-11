1d91da4c586926cf40f6056d1771c10d90ee1dec
Caio-Prado <caio100xa@gmail.com>
2022-06-11 11:51:11 -0300

    adicionando reactrouter e criando page de busca

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logostarWars from '../../img/logoStarWars.png'
 
 import "./Header.css"
 
-export default function Header(props) {
+export default function Header() {
+
+    const [search, setSearch] = useState("")
+
     return (
         <div className="header">
             <img src={logostarWars} alt="logo" />
             <form action="">
-                <input type="text" name="search"  id="search"/>
+                <input 
+                type="text" 
+                name="search"  
+                id="search" 
+                onChange={(e) => setSearch(e.target.value)}
+                />
                 <button type="submit">procurar</button>
             </form>
         </div>
--- src/pages/allCharacters/Characters.jsx
+++ src/pages/allCharacters/Characters.jsx
@@ -8,12 +8,13 @@ import CardCharacters from '../../components/characters/CardCharacters';
 export default function AllCharacters() {
 
     const [personagens, setPersonagens] = useState([])
+    const urlAPI = `https://swapi.dev/api/people/?search=`
 
     useEffect( () => { load() }, [] )
 
     async function load() {
         try {
-            const response = await axios.get(`https://swapi.dev/api/people/?page=${1}`)
+            const response = await axios.get(urlAPI)
             setPersonagens(response.data.results)
         }catch(erro) {
             console.log(erro)
